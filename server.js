0218212f61bc94fe12bcdfae97465915f1565fce
Jyotikumari99 <chunniyadav99@gmail.com>
2022-12-27 21:13:00 +0530

    installing  library short id for shorten url

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express=require('express');
+const mongoose=require('mongoose');
 const app=express();
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb://localhost:27017/Url', {
+  useNewUrlParser: true, useUnifiedTopology: true
+})
 app.set('view engine','ejs');
+
 app.get('/',(req,res)=>{
    res.render('index');
+})
+app.post('/shortUrls',(req,res)=>{
+
 })
 app.listen(process.env.PORT||5000);
